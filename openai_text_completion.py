582311636f82a35ccdba455b1f4f47221376eddf
yuetl3mr <kimthach.work@gmail.com>
2024-01-09 21:24:56 +0700

    import constants

--- openai_text_completion.py
+++ openai_text_completion.py
@@ -1,21 +1,20 @@
 import os
 from openai import AzureOpenAI
-
-APIKEY = "6dfa1da1a2ad4165b1ba1e7b0d60b6fe"
-ENDPOINT = "https://sunhackathon18.openai.azure.com"
+import constants
 
 client = AzureOpenAI(
-    api_key = APIKEY,
+    api_key = constants.APIKEY,
     api_version = "2023-12-01-preview",
-    azure_endpoint = ENDPOINT
+    azure_endpoint = constants.ENDPOINT
 )
 
-deployment_name = 'GPT35TURBO' #Model Deployment Name: GPT35TURBO, GPT35TURBO16K, ADA 
 user_input = input()
+
+deployment_name = 'GPT35TURBO' #Model Deployment Name: GPT35TURBO, GPT35TURBO16K, ADA 
+
 response = client.chat.completions.create(
     model=deployment_name,
     messages=[{"role": "system", "content": user_input}]
 )
 
 print(response.choices[0].message.content)
-#
