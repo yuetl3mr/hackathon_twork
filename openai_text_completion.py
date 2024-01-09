8a15ddc6c184a2dc93aa65ad6e5ead80dbcf47a6
yuetl3mr <kimthach.work@gmail.com>
2024-01-09 19:56:23 +0700

    first commit

---
+++ openai_text_completion.py
@@ -0,0 +1,20 @@
+import os
+from openai import AzureOpenAI
+
+APIKEY = "6dfa1da1a2ad4165b1ba1e7b0d60b6fe"
+ENDPOINT = "https://sunhackathon18.openai.azure.com"
+
+client = AzureOpenAI(
+    api_key = APIKEY,
+    api_version = "2023-12-01-preview",
+    azure_endpoint = ENDPOINT
+)
+
+deployment_name = 'GPT35TURBO' #Model Deployment Name: GPT35TURBO, GPT35TURBO16K, ADA 
+user_input = input()
+response = client.chat.completions.create(
+    model=deployment_name,
+    messages=[{"role": "system", "content": user_input}]
+)
+
+print(response.choices[0].message.content)
