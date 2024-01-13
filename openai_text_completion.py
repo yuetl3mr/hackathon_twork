9be9b5014e75c00cf5be21ba12cf0256cae9329f
yuetl3mr <kimthach.work@gmail.com>
2024-01-14 00:18:18 +0700

    config quiz input

--- openai_text_completion.py
+++ openai_text_completion.py
@@ -8,7 +8,7 @@ client = AzureOpenAI(
     azure_endpoint = constants.ENDPOINT
 )
 
-user_input = input()
+user_input = "Tạo cho tôi 10 câu hỏi trắc nghiệm dựa theo data / yêu cầu sau [bánh mì]. Lưu ý ngôn ngữ tiếng việt và phải theo form sau : Câu hỏi/n Đáp án a/n Đáp án b/n Đáp án c/n Đáp án d/n Đáp án đúng/n Giải thích/n"
 deployment_name = 'GPT35TURBO' #Model Deployment Name: GPT35TURBO, GPT35TURBO16K, ADA 
 
 response = client.chat.completions.create(
